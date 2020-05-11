ba33728ecb5529e6573192eddb5acd51ab648a7a
nikolaims <n.m.smetanin@gmail.com>
2020-05-11 19:57:16 +0300

    test on real data stream sim

--- simulate_eeg_device.py
+++ simulate_eeg_device.py
@@ -2,8 +2,10 @@ from settings import NFBLAB_PATH
 import sys
 sys.path.insert(0, NFBLAB_PATH)
 
+import scipy.signal as sg
 from pynfb.generators import run_eeg_sim
-
+import numpy as np
 if __name__ == '__main__':
-    run_eeg_sim(500, name='EEG_Data')
+    eeg = np.load('data/eeg_example_2_45Hz.npz')
+    run_eeg_sim(eeg['fs'], name='EEG_Data', source_buffer=eeg['data'].T, labels=list(eeg['channels']))
 
